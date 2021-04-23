cda2e13d43a2c25edb6809aa273024fbf93ce5a2
Claudia Rios Alvarez <58591730+claudiariosalvarez@users.noreply.github.com>
2021-04-23 21:42:04 +0200

    Init react project

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AboutMe from './components/aboutMe'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AboutMe /> 
+  </React.StrictMode>,
+  document.getElementById('root')
+);
